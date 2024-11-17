d549b796ce4a407e9235be27a2ddb1138e8bdc81
muzafarali <muzafaralis210@gmail.com>
2024-11-17 17:37:08 +0500

    Pattern new code

---
+++ nestedLoopPattern 8th week work/Array/Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Numbers {
+public static void main(String [] args){
+Scanner s = new Scanner(System.in);
+int num [] = new int[5];
+int result = 0;
+for(int i=0; i<num.length; i++){
+System.out.print("Enter "+i+" : value = ");
+num[i] = s.nextInt();
+result += num[i];
+}
+System.out.println("Sum of loop "+result);
+}
+}
---
+++ nestedLoopPattern 8th week work/PatternSquareRoot.java
@@ -0,0 +1,12 @@
+class PatternSquareRoot {
+public static void main(String [] args){
+int s =0;
+for(int i=1;i<=5;i++){
+for(int j=5;j>=i;j--){
+System.out.print(" ");}
+for(int k=1;k<=i;k++){
+s = i*i;
+System.out.print(s" ");
+}
+System.out.println();
+}}}
